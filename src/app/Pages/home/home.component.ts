39f67b4f1717a2affa8037111af2eaa7fa64eebe
a.javierleonc <a.javierleonc@gmail.com>
2024-09-20 15:13:38 +0000

    Se crean estructuras basicas de home

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Layout/navbar/navbar.component';
+import { FooterComponent } from "../../Layout/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
