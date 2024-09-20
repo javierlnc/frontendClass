0c72179892240b40f87ad6389f6cc5ffce10f9e1
a.javierleonc <a.javierleonc@gmail.com>
2024-09-20 14:37:18 +0000

    Creacion de proyecto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { ContactoComponent } from './Pages/contacto/contacto.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'contact-us', component: ContactoComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
