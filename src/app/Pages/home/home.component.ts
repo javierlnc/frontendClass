c8427e41a526cbc4bbc93500564e51ce9853d322
javierlnc <javierlnc@icloud.com>
2024-09-22 23:30:03 -0500

    hero y featured terminados

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Layout/navbar/navbar.component';
 import { FooterComponent } from "../../Layout/footer/footer.component";
+import { CardComponent } from "../../Layout/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
