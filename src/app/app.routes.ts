ee880167688040631efe5eee4eb82f38a3928c0d
javierlnc <javierlnc@icloud.com>
2024-09-24 09:29:30 -0500

    Correcion de codigo y creacion de pagenotfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutUsComponent } from './Pages/about-us/about-us.component';
 import { ContactoComponent } from './Pages/contacto/contacto.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegistroComponent } from './Pages/registro/registro.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'contact-us', component: ContactoComponent },
     { path: 'login', component: LoginComponent },
     {path: 'registro', component:RegistroComponent},
+    {path: '**', component: PageNotFoundComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
