d4739baf605df5af463d7560e5fe00a7e0495c8b
javierlnc <javierlnc@icloud.com>
2024-09-23 19:17:08 -0500

    Finalizacion about us

--- src/app/Pages/about-us/about-us.component.ts
+++ src/app/Pages/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Layout/navbar/navbar.component';
+import { FooterComponent } from "../../Layout/footer/footer.component";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
