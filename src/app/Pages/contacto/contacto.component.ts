e57eecb8152d362c8aa7d23438567457312723ef
javierlnc <javierlnc@icloud.com>
2024-09-23 22:16:09 -0500

    Se termina Login

--- src/app/Pages/contacto/contacto.component.ts
+++ src/app/Pages/contacto/contacto.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Layout/navbar/navbar.component';
-
+import { FooterComponent } from '../../Layout/footer/footer.component';
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
