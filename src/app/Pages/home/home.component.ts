856accc01ec9bd418d518f32bfdf877e8e640661
javierlnc <javierlnc@icloud.com>
2024-09-24 20:10:16 -0500

    Se agregan comentarios

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Layout/navbar/navbar.component';
 import { FooterComponent } from "../../Layout/footer/footer.component";
 import { CardComponent } from "../../Layout/card/card.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, CardComponent],
+  imports: [NavbarComponent, FooterComponent, CardComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/Pages/registro/registro.component.ts
+++ src/app/Pages/registro/registro.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../Layout/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterLink],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
