16df5d3dbd41401f02d97e980ad25d9ac53540fe
javierlnc <javierlnc@icloud.com>
2024-09-24 22:34:49 -0500

    Finalizacion de comentario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,19 @@ import { LoginComponent } from './Pages/login/login.component';
 import { RegistroComponent } from './Pages/registro/registro.component';
 import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
+// Rutas de información
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'about-us', component: AboutUsComponent },
-    { path: 'contact-us', component: ContactoComponent },
-    { path: 'login', component: LoginComponent },
-    {path: 'registro', component:RegistroComponent},
-    {path: '**', component: PageNotFoundComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }, // Ruta de inicio
+  { path: 'about-us', component: AboutUsComponent }, // Ruta de Acerca de nosotros
+  { path: 'contact-us', component: ContactoComponent }, // Ruta de contacto
+
+  // Rutas de autenticación
+  { path: 'login', component: LoginComponent }, // Ruta de login
+  { path: 'registro', component: RegistroComponent }, // Ruta de registro
+
+  // Ruta de página no encontrada
+  { path: '**', component: PageNotFoundComponent },
+
+  // Ruta de redirección para la ruta raíz
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
 ];
